7947f6e3d2935e9ff8a6a229e0a4dd01772ff302
Dima S <fobius.10@gmail.com>
2020-01-31 16:49:03 +0200

    Create main Activity and Fragment

---
+++ app/src/main/java/com/dmytros/servicelocatorfordi/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.dmytros.servicelocatorfordi
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Dima Skopiuk on 31.01.2020.
+ */
+class MainActivity : AppCompatActivity() {
+
+}
---
+++ app/src/main/java/com/dmytros/servicelocatorfordi/MainFragment.kt
@@ -0,0 +1,8 @@
+package com.dmytros.servicelocatorfordi
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by Dima Skopiuk on 31.01.2020.
+ */
+class MainFragment : Fragment()
